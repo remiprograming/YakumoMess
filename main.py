27935c36ad334499b676ac0263a5868f8b249cb7
RemilSca <kamildudek2001@gmail.com>
2022-03-09 20:42:23 +0100

    v1

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ async def on_ready():
 
     print(f'{disc.user} has connected to Discord!')
 
-    chan = disc.get_channel(951098686425927730)
-
+    chant1 = disc.get_channel(951098686425927730)
+    chantele = disc.get_channel(951181520767438898)
 
 
     if os.path.isfile(f'cookie.txt'):
@@ -42,12 +42,59 @@ async def on_ready():
 
     for event in listener.listen():
         if isinstance(event, fbchat.MessageEvent):
-            print(f"{event.message.text} from {event.author.name} in {event.thread.id}")
-            # nad = fbchat.User(session= x.session, id=event.author.id)
-            # await chan.send(f'{event}')
-            # If you're not the author, echo
             if event.author.id != session.user.id:
-                event.thread.send_text(event.message.text)
+                print(f"{event.message.text} from {event.author.id} in {event.thread.id}")
+
+                t = x.fetch_threads(limit=50)
+
+                for a in t:
+                    if a.id == event.thread.id:
+                        if a.id == '3315449928542074':
+                            ids = []
+                            nickn = a.participants
+                            for users in nickn:
+                                ids.append(users.id)
+
+                            user = nickn[ids.index(event.author.id)]
+                            info = x._fetch_info(user.id)
+                            nick = info[event.author.id]['name']
+                            obrz = []
+                            obrz = event.message.attachments
+                            ur = f''
+                            for obr in obrz:
+                                ur += f'{x.fetch_image_url(obr.id)}\n'
+                            if event.message.text is None:
+                                print(f'{nick}: {ur}')
+                                await chant1.send(f'{nick}: {ur}')
+                            else:
+
+                                print(f'{nick}: {event.message.text}')
+                                await chant1.send(f'{nick}: {ur} {event.message.text}')
+
+
+                        elif a.id == '3199744420124369':
+                            ids = []
+                            nickn = a.participants
+                            for users in nickn:
+                                ids.append(users.id)
+
+                            user = nickn[ids.index(event.author.id)]
+                            info = x._fetch_info(user.id)
+                            nick = info[event.author.id]['name']
+                            obrz = []
+                            obrz = event.message.attachments
+                            ur = f''
+                            for obr in obrz:
+                                ur += f'{x.fetch_image_url(obr.id)}\n'
+                            if event.message.text is None:
+                                print(f'{nick}: {ur}')
+                                await chantele.send(f'{nick}: {ur}')
+                            else:
+
+                                print(f'{nick}: {event.message.text}')
+                                await chantele.send(f'{nick}: {ur} {event.message.text}')
+
+
 
 disc.run(pswd[2])
 
